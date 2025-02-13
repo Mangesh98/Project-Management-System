ae2a1a22ee7dd0c23364f8dd6dcb9204d1e7d552
mangesh98 <mangeshraje555555@gmail.com>
2025-02-13 21:37:10 +0530

    Comment feature implemented

---
+++ Backend/src/main/java/com/mk/ProjectManagementSystem/Config/JwtConstant.java
@@ -0,0 +1,9 @@
+package com.mk.ProjectManagementSystem.Config;
+
+import com.zaxxer.hikari.pool.HikariProxyCallableStatement;
+
+public class JwtConstant {
+    public static final String JWT_SECRET = "ThisIsTempJWT_SECRET_ThisIsTempJWT_SECRET_ThisIsTempJWT_SECRET_ThisIsTempJWT_SECRET_ThisIsTempJWT_SECRET_ThisIsTempJWT_SECRET_ThisIsTempJWT_SECRET_ThisIsTempJWT_SECRET_";
+
+    public static final String JWT_HEADER = "Authorization";
+}
